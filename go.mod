6fc09e97d9fcb7e46c6effec8fcb26e77ef96f12
Hemant Vallabh <hemant.vallabh@outlook.com>
2021-06-16 18:29:12 +0200

    chore: initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hemantvallabh/easyequities-scraper
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("Starting up....")
+
+	//os.Getenv("EE_UID")
+	//os.Getenv("EE_PID")
+}
